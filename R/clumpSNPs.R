4947ead2adada6ca17dc09eba4ae7ae49d7f96f9
Nousiainen Kari <kari.nousiainen@aalto.fi>
2017-11-09 15:37:25 +0200

    Clumping Tested

--- R/clumpSNPs.R
+++ R/clumpSNPs.R
@@ -1,23 +1,41 @@
-library(tidyverse)
-removeHighlyCorrelatedSNPs <- function(proxyPathPrefix,SNPsPath,outputPath,corrCoeff,distance) {
+#' Title
+#'
+#' @param plinkPathPrefix 
+#' @param SNPsPath 
+#' @param outputPath 
+#' @param corrCoeff 
+#' @param distance 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clumpSNPs <- function(plinkPathPrefix,SNPsPath,outputPath,corrCoeff,distance) {
+  library(tidyverse)
+  
     snps <- as.data.frame(read_tsv(SNPsPath,col_names="SNP",col_types = "c"))
     P=rep(0,length=nrow(snps))
     snps <- cbind(snps,P)
-    tmpfilename <- paste(SNPsPath,"tmp.assoc",sep=".")
-    tmpfilename2 <- paste(tmpfilename,"clumped",sep=".")
+    
+    print('SNPS read')
+    tmpfilename <- tempfile(fileext = '.txt')
+
     write_tsv(snps,tmpfilename)
-    cmdstr <- sprintf('plink --bfile %s --clump %s --clump-r2 %f --clump-kb %i --out %s',proxyPathPrefix,
+    print('TMP file  created')
+    cmdstr <- sprintf('plink --bfile %s --clump %s --clump-r2 %f --clump-kb %i --out %s',plinkPathPrefix,
                       tmpfilename,
                       corrCoeff,
                       distance,
-                      tmpfilename2)
+                      tmpfilename)
+    print(cmdstr)
     system(cmdstr)
-    res <- read.table(tmpfilename2,stringsAsFactors = F)
+    
+    res <- read.table(paste(tmpfilename,'clumped',sep="."),stringsAsFactors = F)
     colnames(res)<-res[1,]
     res <- res[-1,]
     nonclumpedSNPs <- as.data.frame(res$SNP)
     write_tsv(nonclumpedSNPs,outputPath,col_names = F)
-  
+    print(sprintf('Clumped SNPs written to %s',outputPath))  
     
 }
   
